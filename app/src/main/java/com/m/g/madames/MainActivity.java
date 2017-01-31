881333c1d2a6a6c56988a8d0b9733059d56c8464
G <gremski@gmail.com>
2017-02-01 00:57:45 +0100

    Initial commit

---
+++ app/src/main/java/com/m/g/madames/MainActivity.java
@@ -0,0 +1,58 @@
+package com.m.g.madames;
+
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private boolean inLs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_main);
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+            inLs = true;
+        }
+        else {
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if (inLs) {
+            mMap = googleMap;
+
+            // Add a marker in Sydney and move the camera
+            LatLng madames = new LatLng(50.051807, 19.942376999999965);
+            mMap.addMarker(new MarkerOptions().position(madames).title("MadameS Shop"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(madames, 15));
+        }
+        else {
+
+        }
+    }
+}
